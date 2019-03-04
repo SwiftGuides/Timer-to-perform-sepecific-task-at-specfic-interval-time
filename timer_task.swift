461b907239b91baf4c07f45fdcd6894a38daf4f6
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-03-04 17:35:41 +0530

    Update timer_task.swift

--- timer_task.swift
+++ timer_task.swift
@@ -1,14 +1,32 @@
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-         //call the time interval method to perfomr the task at evry 60 seconds
-         Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(ViewController.sayHello), userInfo: nil, repeats: true)
-         
-         }
-     
+    weak var timer: Timer?
+
+    override func viewDidAppear(_ animated: Bool) {
+    
+        startTimer() //Starts The timer
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        stopTimer() //Stops the Timer
+    }
+
+    func startTimer(){
+     //call the time interval method to perfomr the task at evry 60 seconds
+         Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(ViewController.sayHello), userInfo: nil, repeats: true)   
+    }
+    
      //Make Objc Functions of th task you want to perform
      @objc func sayHello() 
      {
      NSLog("hello World")
      }
              
+
+    //MARK: Stop Timer 
+    func stopTimer() {
+        timer?.invalidate()
+    }
+    
+    //Optional Case
+    deinit {
+        stopTimer()
+    }
